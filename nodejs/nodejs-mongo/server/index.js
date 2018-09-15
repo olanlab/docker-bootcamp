705a3fbf5e7fe5884a9f794693778969fe09e58c
Olan Samritjiarapon <olan@olanlab.com>
2018-09-15 13:33:54 +0700

    updated docker-bootcamp

--- nodejs/nodejs-mongo/server/index.js
+++ nodejs/nodejs-mongo/server/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://" + process.env.DATABASE_HOST + ":27017";
 
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+	MongoClient.connect(
+		url,
+		(err, client) => {
+			if (err) throw err;
+			console.log("Database connected!");
+
+			const db = client.db("shopper");
+			db.collection("users").findOne({ status: "pending" }, function(err, result) {
+				if (err) throw err;
+				res.send(result);
+
+				client.close();
+			});
+		}
+	);
 });
 
 app.listen(8080, () => {
