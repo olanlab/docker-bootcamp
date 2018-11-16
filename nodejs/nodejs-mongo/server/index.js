96bbfb3fba7212e04e975dd33916bc77e37f7f27
Olan Samritjiarapon <olan@olanlab.com>
2018-11-16 15:45:50 +0700

    updated nodejs-mongo

--- nodejs/nodejs-mongo/server/index.js
+++ nodejs/nodejs-mongo/server/index.js
@@ -3,10 +3,8 @@ const MongoClient = require("mongodb").MongoClient;
 // const url = "mongodb://" + process.env.DATABASE_HOST + ":27017";
 const url = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:27017`;
 
-console.log(url);
-
 const app = express();
-app.get("/", (req, res) => {
+app.get("/api/products", (req, res) => {
 	MongoClient.connect(
 		url,
 		(err, client) => {
@@ -14,13 +12,13 @@ app.get("/", (req, res) => {
 			console.log("Database connected!");
 
 			const db = client.db("shoppers");
-			db.collection("products").find().toArray((err, result)  => {
-				if (err) throw err;
-				console.log(result);
-				res.send(result);
-
-				client.close();
-			});
+			db.collection("products")
+				.find()
+				.toArray((err, result) => {
+					if (err) throw err;
+					res.status(200).send(result);
+					client.close();
+				});
 		}
 	);
 });
