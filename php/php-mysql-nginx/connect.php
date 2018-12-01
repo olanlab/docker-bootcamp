de3b88a11695b2b5b4d418610fa1ab539c04308b
Olan Samritjiarapon <olan@olanlab.com>
2018-12-01 20:28:19 +0700

    updated php-mysql-nginx

---
+++ php/php-mysql-nginx/connect.php
@@ -0,0 +1,12 @@
+<?php 
+$host = 'database';
+$user = 'root';
+$pass = '1111';
+ 
+$conn = mysqli_connect($host, $user, $pass);
+if (!$conn) {
+    exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+}
+ 
+echo 'Successful database connection!'.PHP_EOL;
+
